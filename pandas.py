f7ba14763d3e8f61ba922805763bce8acd05c215
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-09-24 21:10:16 +0530

    Add files via upload

---
+++ pandas.py
@@ -0,0 +1,18 @@
+import pandas as pd
+a = [1,7,2]
+myvar = pd.Series(a)
+print(myvar)
+
+'''
+0    1
+1    7
+2    2
+dtype: int64
+'''
+
+import pandas as pd
+a = [1,7,2]
+myvar = pd.Series(a)
+print(myvar[0])
+
+#O/P - 1
